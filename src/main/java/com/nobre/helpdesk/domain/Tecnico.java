954b1b40ac2322ac56f3f9e43fe89bf5d0635e3e
Nobre 81 <jnobr3c+git@gmail.com>
2024-05-09 00:43:26 -0300

    :construction: Endpoint create para Tecnico

--- src/main/java/com/nobre/helpdesk/domain/Tecnico.java
+++ src/main/java/com/nobre/helpdesk/domain/Tecnico.java
@@ -2,11 +2,13 @@ package com.nobre.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.nobre.helpdesk.domain.dtos.TecnicoDTO;
 import com.nobre.helpdesk.domain.enums.Perfil;
 
 @Entity
@@ -26,6 +28,17 @@ public class Tecnico extends Pessoa{
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.CLIENTE);
 	}
+	
+	public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome =  obj.getNome();
+		this.cpf =  obj.getCpf();
+		this.email =  obj.getEmail();
+		this.senha =  obj.getSenha();
+		this.perfis =  obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao =  obj.getDataCriacao();
+	}
 
 	public List<Chamado> getChamados() {
 		return chamados;
@@ -33,8 +46,6 @@ public class Tecnico extends Pessoa{
 
 	public void setChamados(List<Chamado> chamados) {
 		this.chamados = chamados;
-	}
-	
-	
+	}	
 
 }
