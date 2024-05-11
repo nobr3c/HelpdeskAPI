756266d93fd09b3141f817c476a90c9c347fb277
Nobre 81 <jnobr3c+git@gmail.com>
2024-05-11 18:47:36 -0300

    :construction: Metodo DELETE para Tecnico implementado

--- src/main/java/com/nobre/helpdesk/services/TecnicoService.java
+++ src/main/java/com/nobre/helpdesk/services/TecnicoService.java
@@ -47,6 +47,15 @@ public class TecnicoService {
 		oldObj = new Tecnico(objDTO);
 		return repository.save(oldObj);
 	}
+	
+	public void delete(Integer id) {
+		Tecnico obj =  findById(id);
+		
+		if(obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("O Técnico possui ordens de serviço e não pode ser deletado!");
+		}		
+		repository.deleteById(id);
+	}
 
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
