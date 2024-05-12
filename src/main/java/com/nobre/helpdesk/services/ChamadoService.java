f3d7c186452a9aa3921422293c59fb7a6538c496
Nobre 81 <jnobr3c+git@gmail.com>
2024-05-12 11:36:47 -0300

    :construction: create para chamado

--- src/main/java/com/nobre/helpdesk/services/ChamadoService.java
+++ src/main/java/com/nobre/helpdesk/services/ChamadoService.java
@@ -1,12 +1,20 @@
 package com.nobre.helpdesk.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nobre.helpdesk.domain.Chamado;
+import com.nobre.helpdesk.domain.Cliente;
+import com.nobre.helpdesk.domain.Tecnico;
+import com.nobre.helpdesk.domain.dtos.ChamadoDTO;
+import com.nobre.helpdesk.domain.enums.Prioridade;
+import com.nobre.helpdesk.domain.enums.Status;
 import com.nobre.helpdesk.repositories.ChamadoRepository;
 import com.nobre.helpdesk.services.exception.ObjectnotFoundException;
 
@@ -14,7 +22,11 @@ import com.nobre.helpdesk.services.exception.ObjectnotFoundException;
 public class ChamadoService {
 	
 	@Autowired
-	private ChamadoRepository repository;
+	private ChamadoRepository repository;	
+	@Autowired
+	private TecnicoService tecnicoService;
+	@Autowired
+	private ClienteService clienteService;
 	
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
@@ -24,5 +36,31 @@ public class ChamadoService {
 	public List<Chamado> findAll(){
 		return repository.findAll();
 	}
+
+	public Chamado create(@Valid ChamadoDTO objDTO) {		
+		return repository.save(newChamado(objDTO));
+	}
+	
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+		
+		Chamado chamado = new Chamado();
+		if(obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+		
+		if(obj.getStatus().equals(2)){
+			chamado.setDataFechamento(LocalDate.now());
+		}
+		
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		return chamado;
+	}	 
 
 }
